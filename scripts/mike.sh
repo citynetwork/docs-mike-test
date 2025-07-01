b8e8dc613b7cdbb280ac53bfbaa1b14ba08959f7
Florian Haas <florian.haas@cleura.com>
2025-07-01 17:22:24 +0200

    fix: Add --allow-empty to mike.sh
    Sometimes we want to build two separate mike "versions" for branches
    that differ only in name, not in content.

--- scripts/mike.sh
+++ scripts/mike.sh
@@ -3,5 +3,5 @@
 DOCS_DEPLOY_DEFAULT_BRANCH="${DOCS_DEPLOY_DEFAULT_BRANCH:-main}"
 DOCS_DEPLOY_BRANCH="${DOCS_DEPLOY_BRANCH:-`git rev-parse --abbrev-ref HEAD`}"
 
-mike deploy $DOCS_DEPLOY_BRANCH "$@"
+mike deploy $DOCS_DEPLOY_BRANCH --allow-empty "$@"
 mike set-default $DOCS_DEPLOY_DEFAULT_BRANCH "$@"
